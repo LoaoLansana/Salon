db74ffe853b64ca439184a256e10e7d7623ddc62
João Alexandre Lansana <j.lansana@reply.com>
2024-01-20 20:34:01 -0300

    Criada tela e controller para cadastro de clientes

--- Salao.Repository/Profiles/SalaoProfile.cs
+++ Salao.Repository/Profiles/SalaoProfile.cs
@@ -3,12 +3,6 @@ using Microsoft.AspNetCore.Identity;
 using Salao.Repository.Models;
 using Salao.Repository.Models.Menus.View;
 using Salao.Repository.Models.Usuario.Views;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Salao.Repository.Profiles
 {
